9ea48c7987deadd249a3971d6f2bd96e6917ce0c
Lee-jiyoung <izzzzzi099@gmail.com>
2020-08-27 22:31:15 +0900

    .

--- 200827.py
+++ 200827.py
@@ -10,8 +10,8 @@ def cal(n:int) -> int:
     d = [[0,0,0,0,0,0,0,0,0,0],[1,1,1,1,1,1,1,1,1,1]]
     for i in range(1,n):
         t = list([sum(d[i],j+1) for j in 10])
-        #for j in range(10):
-         #  t.append(sum(d[i],j+1))
+        for j in range(10):
+           t.append(sum(d[i],j+1))
         d.append(t)
     return sum(d[n],10)
 
