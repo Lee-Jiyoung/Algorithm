20229769fc59f26648f3b5807253a6afb9b078f4
Lee-jiyoung <izzzzzi099@gmail.com>
2020-08-27 19:08:58 +0900

    200827200827200827200827200827200827200827200827200827200827200827200827200827200827200827200827200827200827200827200827

---
+++ 200827.py
@@ -0,0 +1,23 @@
+def sum(l:list,n) -> int:
+    s = 0
+    for i in range(n):
+        s += l[i]
+    return s
+
+def cal(n:int) -> int:
+    d = [[0,0,0,0,0,0,0,0,0,0],[1,1,1,1,1,1,1,1,1,1]]
+    for i in range(1,n):
+        t = list([sum(d[i],j+1) for j in 10])
+        #for j in range(10):
+         #  t.append(sum(d[i],j+1))
+        d.append(t)
+    return sum(d[n],10)
+
+cn = int(input())
+res = []
+
+for i in range(cn):
+    t = int(input())
+    res.append(cal(t))
+
+print("\n".join(str(i) for i in res))
